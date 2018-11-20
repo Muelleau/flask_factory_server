051c8f4b72e5d6aa87e78b6d04738cc0bfe83c9d
andrew <Muelleau1917@gmail.com>
2018-11-19 22:34:08 -0500

    my skeleton app

---
+++ app/main/config.py
@@ -0,0 +1,50 @@
+import os
+
+# uncomment the line below for postgres database url from environment variable
+# postgres_local_base = os.environ['DATABASE_URL']
+
+basedir = os.path.abspath(os.path.dirname(__file__))
+
+class Config:
+    SECRET_KEY = os.getenv('SECRET_KEY', 'my_precious_secret_key')
+    DEBUG = False
+
+
+class DevelopmentConfig(Config):
+    # uncomment the line below to use postgres
+    # SQLALCHEMY_DATABASE_URI = postgres_local_base
+    DEBUG = True
+
+    POSTGRES = {
+    'user': 'postgres',
+    'pw': '',
+    'db': 'postgres',
+    'host': 'localhost',
+    'port': '32776',
+    }
+    SQLALCHEMY_DATABASE_URI = 'postgresql://%(user)s:%(pw)s@%(host)s:%(port)s/%(db)s' % POSTGRES
+    SQLALCHEMY_TRACK_MODIFICATIONS = False
+
+
+class TestingConfig(Config):
+    DEBUG = True
+    TESTING = True
+    SQLALCHEMY_DATABASE_URI = "postgresql:://postgres@localhost:32776/dev"
+    PRESERVE_CONTEXT_ON_EXCEPTION = False
+    SQLALCHEMY_TRACK_MODIFICATIONS = False
+
+
+class ProductionConfig(Config):
+    DEBUG = False
+    SQLALCHEMY_DATABASE_URI = "postgresql:://postgres@localhost:32776/dev"
+    # uncomment the line below to use postgres
+    # SQLALCHEMY_DATABASE_URI = postgres_local_base
+
+
+config_by_name = dict(
+    dev=DevelopmentConfig,
+    test=TestingConfig,
+    prod=ProductionConfig
+)
+
+key = Config.SECRET_KEY
---
+++ setup.py
@@ -0,0 +1,20 @@
+
+
+from setuptools import setup, find_packages 
+from os import path 
+from io import open 
+
+
+here = path.abspath(path.dirname(__file__))
+
+with open(path.join(here, "README.md"), encoding='utf-8') as f:
+        long_description = f.read()
+
+setup(
+    name='package_name',
+    version=1.0, 
+    description='an api',
+    long_description=long_description,
+    long_description_content_type='text/markdown',
+    author='me',
+)
